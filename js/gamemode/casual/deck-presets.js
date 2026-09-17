5f519648cf6d786fce644095b85afde9344a8331
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2026-09-17 10:35:52 -0300

    style: padroniza cores dos modais e presets de baralho
    - Substitui o gradiente da classificação pelo fundo sólido do perfil
    - Iguala o escurecimento dos modais de classificação e perfil ao casual
    - Padroniza as cores dos botões de presets com as configurações
    - Ajusta o preset Base + DLC 1 para incluir 5 Marionetistas
    - Atualiza versões dos arquivos para invalidar o cache

--- js/gamemode/casual/deck-presets.js
+++ js/gamemode/casual/deck-presets.js
@@ -58,9 +58,7 @@
           break;
 
         case 'base_dlc1':
-          setInputValue(input, card === 'marionetista'
-            ? 3
-            : isInGroups(card, ['base', 'dlc1']) ? 5 : 0);
+          setInputValue(input, isInGroups(card, ['base', 'dlc1']) ? 5 : 0);
           break;
 
         case 'base_dlc2':
